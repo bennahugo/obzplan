2cc11db4648dcbb85d8f9d637ba0ef960052cda5
Benna Hugo <bennahugo@aol.com>
2017-12-08 15:03:41 +0200

    Added more sources

--- obs_planner.py
+++ obs_planner.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import argparse
 from mpl_toolkits.mplot3d import Axes3D
-
+import datetime
 
 parser = argparse.ArgumentParser(description="Observation Planner")
 parser.add_argument("--lat",
@@ -46,12 +46,32 @@ scp = ephem.readdb("SCP,f|J,00:00:00.00000,-90:00:00:00000,0.0")
 sources = {"Sun": ephem.Sun(),
            "DEEP2": ephem.readdb("DEEP_2,f|J,04:13:26.40,-80.00.00.00000,0.0"),
            "0252-712": ephem.readdb("0252-712,f|J,02:52:46.15,-71.04.35.30,0.0"),
-           "0408-65": ephem.readdb("0408-65,f|J,04:08:20:38,-65.45.09.10,0.0")
+           "0408-65": ephem.readdb("0408-65,f|J,04:08:20:38,-65.45.09.10,0.0"),
+           "PKS 1934-638":
+           ephem.readdb("custom,f|J,19:39:25:00,-63.42.46.00,0.0"),
+           "PKS 1934-638": ephem.readdb("custom,f|J,19:39:25:00,-63.42.46.00,0.0"),
+           "3C286": ephem.readdb("custom,f|J,13:31:08.3,30:30:33,0.0"),
+           "3C138": ephem.readdb("custom,f|J,05:21:09,16:38:22,0.0"),
+           "PKS 1422-29": ephem.readdb("custom,f|J,14:25:30:00,-30.00.00.00,0.0"),
+           "alleged ghost": ephem.readdb("custom,f|J,14:25:30:00,30.00.00.00,0.0"),
+           "1244-255": ephem.readdb("custom,f|J,12:46:46:80,-25:47:49:3,0.0"),
+           "1334-127": ephem.readdb("custom,f|J,13:37:39:78,-12:57:24:7,0.0"),
+           "NGC4993_off": ephem.readdb("custom,f|J,13:09:21:00,-23:31:24:0,0.0"),
+           "3C283": ephem.readdb("custom,f|J,13:11:40:10,-22:17:04:0,0.0"),
           }
 positions = {"Sun": [],
              "DEEP2": [],
              "0252-712": [],
-             "0408-65": []
+             "0408-65": [],
+             "PKS 1934-638": [],
+             "3C286": [],
+             "3C138": [],
+             "PKS 1422-29": [],
+             "alleged ghost": [],
+             "1244-255": [],
+             "1334-127": [],
+             "NGC4993_off": [],
+             "3C283": []
             }
 
 pi = np.pi
@@ -78,6 +98,8 @@ for t in np.linspace(st,et,1500):
     meerkat.date = t
     for k in sources:
         sources[k].compute(meerkat)
+        if k == "Sun":
+            print sources[k].ra, sources[k].dec
         positions[k].append([float(sources[k].az),
                              float(np.pi * 0.5 - sources[k].alt)])
 
@@ -114,6 +136,7 @@ for s in args.sources:
             np.sin(np.deg2rad(positions[s][:,1]))*np.sin(np.deg2rad(positions[s][:,0])),
             np.cos(np.deg2rad(positions[s][:,1])), label=s)
 ax.legend()
+ax.grid(False)
 ax.set_xlim(-1,1)
 ax.set_ylim(-1,1)
 ax.set_zlim(-1,1)
